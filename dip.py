61660335f9deb397b86e7b2fb975099eef620451
fotios <fotios@fundalytica.com>
2020-11-30 16:00:49 +0000

    organize imports

--- dip.py
+++ dip.py
@@ -27,9 +27,8 @@ class DataContainer:
         if verbose:
             utils.cprint(file, Fore.CYAN)
 
-        date = DATA_COLUMNS[provider]['date']
+        index = DATA_COLUMNS[provider]['date']
         close = DATA_COLUMNS[provider]['close']
-        index = date
 
         # read dataframe from file
         df = data.df_read(file, index=index)
@@ -53,150 +52,37 @@ class DataContainer:
             dict = {value:key for (key, value) in DATA_COLUMNS[provider].items()}
             df.rename(columns=dict, inplace=True)
 
-        if verbose:
-            print(df)
-
         self.df = df
 
 def ath_df(df):
-    column = 'close'
-    mask = (df[column] == df.cummax()[column])
-    return df[mask]
+    close = 'close'
 
-def ath_locs(df):
-    indices = ath_df(df).index.tolist()
-    locs = list(map(lambda index: df.index.get_loc(index), indices))
-    return locs
+    m = (df[close] == df.cummax()[close])
+    ath_df = df[m]
 
-# keep only last of consecutive all time high indices
-def remove_adjacent_indices(indices):
-    def method(index_value):
-        index = indices.index(index_value)
+    return ath_df
 
-        if index == 0 or index == len(indices) - 1:
-            return True
+def dip_df(df, threshold):
+    close = 'close'
 
-        return ((indices[index] + 1) != indices[index + 1])
+    dip_df = df.copy()                                      # make copy
+    dip_df['ath'] = dip_df.cummax()                         # add all time high
+    dip_df['dip'] = dip_df[close] / dip_df['ath'] - 1       # add dip percentage
 
-    return list(filter(method, indices))
+    # https://towardsdatascience.com/pandas-dataframe-group-by-consecutive-certain-values-a6ed8e5d8cc
+    dip_df['close-eq-ath'] = dip_df[close] == dip_df['ath'] # boolean column (close price equal to all time high)
+    dip_df['segment'] = dip_df['close-eq-ath'].cumsum()     # segment number (cumulative sum of boolean values)
+    segments = dip_df.groupby('segment')                    # group to create one segment for each ath
 
-# indices pointing to qualifying dips
-def dip_indices(symbol, threshold, data, ath_indices):
-    headers = data['headers']
-    rows = data['rows']
-    PRICE_INDEX = headers.index(CLOSE_ID)
+    dip_df = pd.DataFrame()                                 # empty dataframe to add segment dips
+    for num,segment in segments:
+        m1 = segment[close] < segment['ath']                # is not the all time high
+        m2 = segment[close] == segment[close].cummin()      # is the cumulative minimum (lower all than prices before it)
+        m3 = segment['dip'] <= threshold                    # meets the threshold
+        segment = segment[m1 & m2 & m3]
+        dip_df = dip_df.append(segment[[close,'ath','dip']])
 
-    indices = []
-
-    for i in range(len(ath_indices) - 1):
-        # from one all time high to the next
-        from_index = ath_indices[i]
-        to_index = ath_indices[i + 1]
-
-        lowest_index = from_index
-        for j in range(from_index, to_index + 1):
-            price = rows[j][PRICE_INDEX]
-            lowest_price = rows[lowest_index][PRICE_INDEX]
-
-            # new lower price
-            if price < lowest_price:
-                lowest_index = j
-
-        from_price = rows[from_index][PRICE_INDEX]
-        lowest_price = rows[lowest_index][PRICE_INDEX]
-        difference = from_price - lowest_price
-        percentage = difference / from_price * 100
-        if difference:
-            if percentage >= threshold:
-                indices.append(lowest_index)
-
-    return indices
-
-# drop from an all time high in percentage terms
-def dip_drop_percentage(data, dip_index, ath_indices):
-    headers = data['headers']
-    rows = data['rows']
-    PRICE_INDEX = headers.index(CLOSE_ID)
-
-    from_ath_index = list(filter(lambda index: index < dip_index, ath_indices))[-1]
-    from_ath_price = rows[from_ath_index][PRICE_INDEX]
-    dip_price = rows[dip_index][PRICE_INDEX]
-
-    drop = dip_price / from_ath_price - 1
-
-    return drop
-
-# verbose dips description
-def dips_description(symbol, data, ath_indices, dip_indices):
-    print(f'{Fore.CYAN}{symbol} Dips > {threshold}%{Style.RESET_ALL}')
-
-    DATE_OUT_FORMAT = '%b %d, %Y'
-
-    headers = data['headers']
-    rows = data['rows']
-    PRICE_INDEX = headers.index(CLOSE_ID)
-    DATE_INDEX = headers.index(DATE_ID)
-
-    for i in range(len(dip_indices)):
-        dip_index = dip_indices[i]
-        from_index = list(filter(lambda index: index < dip_index, ath_indices))[-1]
-        to_index = list(filter(lambda index: index > dip_index, ath_indices))[0]
-
-        start_date = datetime.strptime(rows[from_index][DATE_INDEX], DATE_FORMAT)
-        start_price = rows[from_index][PRICE_INDEX]
-        lowest_date = datetime.strptime(rows[dip_index][DATE_INDEX], DATE_FORMAT)
-        lowest_price = rows[dip_index][PRICE_INDEX]
-        last_date = datetime.strptime(rows[to_index][DATE_INDEX], DATE_FORMAT)
-        last_price = rows[to_index][PRICE_INDEX]
-
-        drop = dip_drop_percentage(data, dip_index, ath_indices)
-        rise = last_price / lowest_price - 1
-
-        days = (last_date - start_date).days
-        months = days / 365 * 12
-        years = days / 365
-
-        dates_string = f'{start_date.strftime(DATE_OUT_FORMAT)} to {last_date.strftime(DATE_OUT_FORMAT)}'
-        duration_string = f'{days}d {months:.1f}m {years:.1f}y'
-        print(f'ATH Range {Fore.BLUE}{dates_string} {duration_string}{Style.RESET_ALL}')
-
-        price_string = f'{start_price}'
-        lowest_price_string = f'{lowest_price}'
-        drop_string = f'{drop:,.2%} on {lowest_date.strftime(DATE_OUT_FORMAT)} took {(lowest_date - start_date).days}d'
-        print(f'Price {price_string} drop to {lowest_price_string} {Fore.RED}{drop_string}{Style.RESET_ALL}')
-
-        last_price_string = f'{last_price}'
-        recovery_string = f'+{rise:,.2%} took {(last_date - lowest_date).days}d'
-        print(f'Recovery at {last_price_string} {Fore.GREEN}{recovery_string}{Style.RESET_ALL}')
-
-# get days since first day from indices
-def indices_days(indices, data):
-    DATE_INDEX = data['headers'].index(DATE_ID)
-    first_date = datetime.strptime(data['rows'][0][DATE_INDEX], DATE_FORMAT)
-
-    dates = list(map(lambda index: data['rows'][index][DATE_INDEX], indices))
-    # fmt_dates = list(map(lambda date: date.strftime(format), py_dates))
-    py_dates = list(map(lambda date: datetime.strptime(date, DATE_FORMAT), dates))
-    days = list(map(lambda date: (date - first_date).days, py_dates))
-
-    return days
-
-# get prices from indices
-def indices_prices(indices, data):
-    PRICE_INDEX = data['headers'].index(CLOSE_ID)
-    return list(map(lambda index: data['rows'][index][PRICE_INDEX], indices))
-
-def first_date(data):
-    headers = data['headers']
-    rows = data['rows']
-    DATE_INDEX = headers.index(DATE_ID)
-    return datetime.strptime(data['rows'][0][DATE_INDEX], DATE_FORMAT)
-
-def last_date(data):
-    headers = data['headers']
-    rows = data['rows']
-    DATE_INDEX = headers.index(DATE_ID)
-    return datetime.strptime(data['rows'][len(data['rows']) - 1][DATE_INDEX], DATE_FORMAT)
+    return dip_df
 
 def draw_plot(data, dip_indices, dip_ath_indices, edge_indices, final_indices):
     fig, ax = plt.subplots()
@@ -278,54 +164,26 @@ provider = args.provider
 verbose = utils.terminal()
 
 data = DataContainer(symbol, provider, verbose=verbose)
+df = data.df
 
-stdout = data.df.to_json(orient='index') if (data.df is not None) else data.df
+ath = ath_df(df)
+dip = dip_df(df, -(threshold / 100))
+
+stdout = df.to_json(orient='columns') if (df is not None) else df
 print(stdout)
 
-# # all time high indices
-# ath_indices = ath_indices(symbol, data)
-# # without consecutive rises (keep last only)
-# # ath_indices = remove_adjacent_indices(ath_indices)
-# # dip indices
-# dip_indices = dip_indices(symbol, threshold, data, ath_indices)
+if verbose:
+    utils.cprint('\nstdout', Fore.YELLOW)
+    utils.json_print(stdout)
 
-# if DEBUG:
-#     dips_description(symbol, data, ath_indices, dip_indices)
+    utils.cprint('\ndf', Fore.YELLOW)
+    print(df)
 
-# # JSON response
-# response = {}
-# response["ath_indices"] = ath_indices
-# response["dip_indices"] = dip_indices
+    utils.cprint('\nath df', Fore.YELLOW)
+    print(ath)
 
-# response["data"] = {}
-# response["data"]["headers"] = data["headers"]
+    utils.cprint('\ndip df', Fore.YELLOW)
+    print(dip)
 
-# # rows = []
-# # for index in range(len(data["rows"])):
-# #     if index in ath_indices or index in dip_indices:
-# #         rows.append(data["rows"][index])
-# rows = data["rows"]
-# rows = list(filter(lambda row: rows.index(row) in ath_indices or rows.index(row) in dip_indices, rows))
-
-# response["data"]["rows"] = rows
-
-# print(response)
-
-# # the two ath indices surrounding each dip index
-# dip_ath_indices = []
-# for dip_index in dip_indices:
-#     from_index = list(filter(lambda index: index < dip_index, ath_indices))[-1]
-#     to_index = list(filter(lambda index: index > dip_index, ath_indices))[0]
-#     dip_ath_indices.extend((from_index, to_index))
-
-# # add first and last if not ath
-# edge_indices = []
-# if dip_ath_indices and dip_ath_indices[0] != 0:
-#     edge_indices.append(0)
-# if dip_ath_indices and dip_ath_indices[len(dip_ath_indices) - 1] != len(data['rows']) - 1:
-#     edge_indices.append(len(data['rows']) - 1)
-
-# # dip indices and its surrounding all time high indices
-# final_indices = sorted(dip_indices + dip_ath_indices + edge_indices)
 
 # draw_plot(data, dip_indices, dip_ath_indices, edge_indices, final_indices)
